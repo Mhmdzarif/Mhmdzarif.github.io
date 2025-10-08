90f78af14b107d59f965ff9165ebaa08f1e6bbc0
Mhmdzarif <mhmdzrf@gmail.com>
2025-10-08 12:17:20 +0300

    Created script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("scroll", () => {
+  document.querySelectorAll(".section").forEach(sec => {
+    const rect = sec.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) sec.classList.add("visible");
+  });
+});
+
